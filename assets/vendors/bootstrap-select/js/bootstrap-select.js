6ccf87ec30aed344abee4a416b0c77e16a40e4e4
chikiet88 <chikiet88@gmail.com>
2023-06-05 16:47:27 +0700

    Auto-generated commit [ci skip]

--- assets/vendors/bootstrap-select/js/bootstrap-select.js
+++ assets/vendors/bootstrap-select/js/bootstrap-select.js
@@ -879,7 +879,7 @@
 
   // part of this is duplicated in i18n/defaults-en_US.js. Make sure to update both.
   Selectpicker.DEFAULTS = {
-    noneSelectedText: 'Vui Lòng Chọn',
+    noneSelectedText: 'Nothing selected',
     noneResultsText: 'No results matched {0}',
     countSelectedText: function (numSelected, numTotal) {
       return (numSelected == 1) ? '{0} item selected' : '{0} items selected';
